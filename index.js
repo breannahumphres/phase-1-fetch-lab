49db05f7526682e821216132b72d05c2e78eb901
Bre Humphres <breannahumphres@gmail.com>
2024-08-14 18:30:44 -0700

    finished fetch lab

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(response => {return response.json()})
+  .then(renderBooks)
+  .catch(error => console.error(error))
 }
-
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
